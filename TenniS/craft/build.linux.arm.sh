9bd16c0c7e691096be773405dbf7b0181a488c8d
cqm <cqm@97kid.com>
2024-04-07 09:22:06 +0800

    add build_linux_arm.sh

---
+++ TenniS/craft/build.linux.arm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export BUILD_DIR=build.linux.x64
+export BUILD_TYPE=Release
+
+export INSTALL_DIR=$(cd "$(dirname "$0")"; pwd)/../../build
+
+HOME=$(cd `dirname $0`; pwd)
+
+cd $HOME
+
+mkdir "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+
+
+cmake "$HOME/.." \
+-DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+-DCONFIGURATION="$BUILD_TYPE" \
+-DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+-DTS_USE_OPENMP=ON \
+-DTS_USE_SIMD=ON \
+-DTS_ON_ARM=ON \
+-DTS_USE_NEON=ON
+
+make -j16
+
+make install
